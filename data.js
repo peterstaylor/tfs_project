575348fdf0f41f6a357107ffea6b6e7a98eb6cd3
peterstaylor <peters.taylor@gmail.com>
2019-03-27 08:27:02 -0400

    making progress on response storage as data objects

---
+++ data.js
@@ -0,0 +1,7 @@
+function data(name, n_in, f_in) {
+  this.name = name; 
+  this.food = f_in;  
+  this.amount = n_in; 
+  //this.costs = costs.push(c_in); 
+  //this.pickup = pickup; */
+}
--- main.js
+++ main.js
@@ -1,25 +1,35 @@
 function main() {
-  var menuform = FormApp.openById('1A3a2zD6GvE2PUE4kRUqDZODzSKnxQaMaEUvzgASDSbY'); 
-  var allitems = menuform.getResponses();  
-  var i, j; 
-  
-  for(i = 0 ; i<allitems.length; i++){
-    for(j = 0; j<allitems[i].getItemResponses(); j++){
-       var responses = allitems[i].getItemResponses(); 
-        /* text answers are for name and for order amounts */ 
+    var menuform = FormApp.openById('1A3a2zD6GvE2PUE4kRUqDZODzSKnxQaMaEUvzgASDSbY'); 
+    var allitems = menuform.getResponses();  
+    var i, j; 
+    var len = allitems.length; 
+    var responses = allitems[len - 1].getItemResponses(); 
+    var output = new data(); 
+    var amount = []; 
+    var food = []; 
+
+    for (j = 0; j < responses.length; j++) {
         if (responses[j].getItem().getType() == FormApp.ItemType.TEXT) {
-        
-      }
-        /* paragraph answers are for comments */ 
+            if (j == 0) {
+                output.name = responses[j].getResponse(); 
+                
+            }
+            else {
+                amount.push(responses[j].getResponse());
+                food.push(responses[j].getItem().getTitle()); 
+            }
+        }
+
         else if (responses[j].getItem().getType() == FormApp.ItemType.PARAGRAPH_TEXT) {
 
-      }
-         /*multiple choice is for drop off location*/ 
+        }
+
         else if (responses[j].getItem().getType() == FormApp.ItemType.MULTIPLE_CHOICE) {
 
         }
     }
-  }
-  
+    output.amount = amount; 
+    output.food = food; 
+    Logger.log(output); 
  
 }
